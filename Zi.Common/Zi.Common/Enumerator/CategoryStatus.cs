c182a79e0754baa2508dd14a6c973e48947fcf41
Hùng Thanh <thanhhung180498@gmail.com>
2023-03-13 23:54:04 +0700

    Create CategoryStatus enum

---
+++ Zi.Common/Zi.Common/Enumerator/CategoryStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zi.Common.Enumerator
+{
+    public enum CategoryStatus
+    {
+        NotAvailable,
+        Available,
+    }
+
+    
+}
